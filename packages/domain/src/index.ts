38fd9d1d96a80937fb3f283d4f2584d5875d0c52
soulzerox <hunterbomber@gmail.com>
2026-09-21 18:48:30 +0700

    feat(#82): export runtime event contract

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -2,6 +2,7 @@ export * from './command.js';
 export * from './errors.js';
 export * from './goal-continuation.js';
 export * from './goal-runtime.js';
+export * from './goal-runtime-event.js';
 export * from './goal-workspace.js';
 export * from './invocation-authorization.js';
 export * from './scheduled-continuation.js';

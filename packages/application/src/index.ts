32c2b46def605802311dcdd7d7ed13479c92d37d
soulzerox <hunterbomber@gmail.com>
2026-09-23 01:14:57 +0700

    feat(#11): export goal worktree provisioning service

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -11,6 +11,7 @@ export * from './goal-request-cancellation-service.js';
 export * from './goal-mutation-fence-service.js';
 export * from './goal-runtime-control-plane-service.js';
 export * from './goal-workspace-truth-reader.js';
+export * from './goal-workspace-service.js';
 export * from './goal-runtime-reconciliation-service.js';
 export * from './managed-resource-recovery-service.js';
 export * from './scheduled-continuation-service.js';
